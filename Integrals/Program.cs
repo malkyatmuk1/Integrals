3bba0c718b61cea79cda3ab8ebfbffbb4338a017
malkyatmuk <tanya.naidenova@abv.bg>
2017-02-12 23:34:09 +0200

    Latex output

--- Integrals/Program.cs
+++ Integrals/Program.cs
@@ -12,17 +12,27 @@ namespace Integrals
         {
             double r;
             int m , n , a = 0, b = 1, c = 0, d = 1;
-            int Rcnt;
- 
-            
+            int Rcnt = 0 ;
+            int l=0;
+           
+            double deltaR = 0.1;
+            double[,] A, B, APlus, BPlus;
+            Rcnt = Convert.ToInt32(1.0d / deltaR) + 1;
+            A = new double[Rcnt, 4];
+            B = new double[Rcnt, 4];
+
+            APlus = new double[Rcnt, 4];
+            BPlus = new double[Rcnt, 4];
+            while(l!=4)
+            {
                 Console.WriteLine("Write m, n, delta, deltaR, so, to");
                m=int.Parse( Console.ReadLine());
                n = int.Parse(Console.ReadLine());
                double delta = double.Parse(Console.ReadLine());
-               double deltaR = double.Parse(Console.ReadLine());
+                deltaR = double.Parse(Console.ReadLine());
                double so = double.Parse(Console.ReadLine());
                double to = double.Parse(Console.ReadLine());
-               Rcnt = Convert.ToInt32(1.0d / deltaR)+1;
+               
                Integral integral = new Integral( a, b, c, d, n, m);
                
                double[,,] F = new double[n+1 , n+1, 2];
@@ -34,9 +44,9 @@ namespace Integrals
                double[] FstPlus = new double[2];
                double[] Fst2Plus = new double[2];
 
-               double[] A = new double[Rcnt], B = new double[Rcnt];
-               double[] APlus = new double[Rcnt], BPlus = new double[Rcnt];
+               
                int count = 0; 
+            
 
                 for (int v = 0; v < Rcnt ; v++)
                 {
@@ -84,29 +94,115 @@ namespace Integrals
                    
                    int k3 = m % 2;
                    
-                   A[count] = Math.Abs((2.0d+r)*so*to-Fst[k3]);
-                   B[count] = Math.Abs(Fst[k3] - Fst2[k3]);
-                   APlus[count] = Math.Abs((4.0d - r) * so * to - FstPlus[ k3]);
-                   BPlus[count] = Math.Abs(FstPlus[k3] - Fst2Plus[k3]);
+                   A[count,l] = Math.Abs((2.0d+r)*so*to-Fst[k3]);
+                   B[count,l] = Math.Abs(Fst[k3] - Fst2[k3]);
+                   APlus[count,l] = Math.Abs((4.0d - r) * so * to - FstPlus[ k3]);
+                   BPlus[count,l] = Math.Abs(FstPlus[k3] - Fst2Plus[k3]);
                    count++;
                 }
+                l++;
+            }
+            using (System.IO.StreamWriter file = 
+            new System.IO.StreamWriter(@"..\..\tables.txt"))
+            {
+                
+                file.WriteLine(@"\begin{table}");
+                file.WriteLine(@"\caption{Numerical results}");
+                file.WriteLine(@"\label{tab:1}       % Give a unique label");
+                file.WriteLine("%");
+                file.WriteLine("% Follow this input for your own table layout");
+                file.WriteLine("%");
+                file.WriteLine("\begin{tabular}{p{2cm}p{2.4cm}p{2cm}p{4.9cm}}");
+                file.WriteLine(@"\hline\noalign{\smallskip}");
+                file.WriteLine(@" r & \multicolumn{2}{c}{m=10 n=10} & \multicolumn{2}{c}{m=20 n=10} & \ multicolumn{2}{c}{m=10 n=30} & multicolumn{2}{c}{m=20 n=30} &   \midrule");
+                file.WriteLine(@"{} & e+ e-  & e+ e-    & e+ e-   & e+ e-\");
+                file.WriteLine(@"\noalign{\smallskip}\svhline\noalign{\smallskip}");
+                double r2 = 0;
 
-
-                double r1=0;
-                Console.WriteLine("+----+-----------+-------+-----------+-------+");
-                Console.WriteLine("| r  |    e-     |  d'-  |    e+     |  d'+  |");
-                Console.WriteLine("+----+-----------+-------+-----------+-------+");
                 for (int i = 0; i < Rcnt; i++)
                 {
-                    Console.WriteLine("|" + r1.ToString("0.00") + "|" + A[i].ToString("0.000000000") + "|" + B[i].ToString("0.00E0") + "|" + APlus[i].ToString("0.000000000") + "|" + BPlus[i].ToString("0.00E0") + "|");
-                    r1 = r1 + deltaR;
+                    file.WriteLine(r2.ToString("0.00") + " & " + A[i, 0].ToString("0.000000000") + " & " + APlus[i, 0].ToString("0.000000000") + @"\ " + A[i, 1].ToString("0.000000000") + " & " + APlus[i, 1].ToString("0.000000000") + @"\ " + A[i, 2].ToString("0.000000000") + " & " + APlus[i, 2].ToString("0.000000000") + @"\ " + A[i, 3].ToString("0.000000000") + " & " + APlus[i, 3].ToString("0.000000000") + @"\");
+                    r2 = r2 + deltaR;
                 }
-                Console.WriteLine("+----+-----------+-------+-----------+-------+");
-           
-                
+
+
+                file.WriteLine(@"\noalign{\smallskip}\hline\noalign{\smallskip}");
+                file.WriteLine(@"\end{tabular}");
+                file.WriteLine("$^a$ Table foot note (with superscript)");
+                file.WriteLine(@"\end{table}");
+                file.WriteLine();
+                //for d
+                file.WriteLine(@"\begin{table}");
+                file.WriteLine(@"\caption{Numerical results}");
+                file.WriteLine(@"\label{tab:1}       % Give a unique label");
+                file.WriteLine("%");
+                file.WriteLine("% Follow this input for your own table layout");
+                file.WriteLine("%");
+                file.WriteLine("\begin{tabular}{p{2cm}p{2.4cm}p{2cm}p{4.9cm}}");
+                file.WriteLine(@"\hline\noalign{\smallskip}");
+                file.WriteLine(@" r & \multicolumn{2}{c}{m=10 n=10} & \multicolumn{2}{c}{m=20 n=10} & \ multicolumn{2}{c}{m=10 n=30} & multicolumn{2}{c}{m=20 n=30} &   \midrule");
+                file.WriteLine(@"{} & d+ d-  & d+ d-    & d+ d-   & d+ d-\");
+                file.WriteLine(@"\noalign{\smallskip}\svhline\noalign{\smallskip}");
+                double r3 = 0;
+
+                for (int i = 0; i < Rcnt; i++)
+                {
+                    file.WriteLine(r2.ToString("0.00") + " & " + B[i, 0].ToString("0.000000000") + " & " + BPlus[i, 0].ToString("0.000000000") + @"\ " + B[i, 1].ToString("0.000000000") + " & " + BPlus[i, 1].ToString("0.000000000") + @"\ " + B[i, 2].ToString("0.000000000") + " & " + BPlus[i, 2].ToString("0.000000000") + @"\ " + B[i, 3].ToString("0.000000000") + " & " + BPlus[i, 3].ToString("0.000000000") + @"\");
+                    r3 = r3 + deltaR;
+                }
+
+
+                file.WriteLine(@"\noalign{\smallskip}\hline\noalign{\smallskip}");
+                file.WriteLine(@"\end{tabular}");
+                file.WriteLine("$^a$ Table foot note (with superscript)");
+                file.WriteLine(@"\end{table}");
+            } 
             }
 
             }
         }
+
+
+
+/*
+4
+4
+0.1
+0.1
+0.5
+0.5
+10
+10
+0.1
+0.1
+0.5
+0.5
+20
+10
+0.1
+0.1
+0.5
+0.5
+10
+30
+0.1
+0.1
+0.5
+0.5
+20
+30
+0.1
+0.1
+0.5
+0.5
+  */
+
+
+
+
+
+
+
+
 
 
